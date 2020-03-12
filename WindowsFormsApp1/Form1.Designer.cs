926dc00a5cb32b6f25218cd8ab1306d2f595063a
lizamashchenko <55406171+lizamashchenko@users.noreply.github.com>
2020-03-12 10:38:26 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 106);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(475, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(475, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(475, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public List<Athlete> athletes = new List<Athlete>();
+        public List<Form> forms = new List<Form>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [Serializable]
+        public class Athlete
+        {
+            public string id;
+            public string Sur;
+            public string Sport;
+            public Athlete(string id, string sur, string sport)
+            {
+                this.id = id;
+                this.Sur = sur;
+                this.Sport = sport;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form form = new Form1();
+            Show(form);
+            forms.Add(form);
+        }
+        public void Save()
+        {
+            foreach(Form f in forms)
+            {
+                Athlete ath = new Athlete(textBox1.Text,textBox2.Text,textBox3.Text);
+                athletes.Add(ath);
+            }
+            XmlSerializer ser = new XmlSerializer(typeof(List<Athlete>));
+            using (FileStream fs = new FileStream("../data.xml", FileMode.OpenOrCreate))
+                ser.Serialize(fs, athletes);
+        }
+        public void LoadData()
+        {
+            List<Athlete> list;
+            XmlSerializer ser = new XmlSerializer(typeof(List<Athlete>));
+            using (FileStream fs = new FileStream("../data.xml", FileMode.Open))
+              list = (List<Athlete>)ser.Deserialize(fs);
+            for (int i = 0; i < forms.Count; i++)
+            {
+                Athlete ath;
+                ath = athletes[i];
+                foreach (Form f in forms)
+                {
+                    TextBox textBox1 = new TextBox();
+                    TextBox textBox2 = new TextBox();
+                    TextBox textBox3 = new TextBox();
+                    textBox1.Text = ath.id;
+                    textBox2.Text = ath.Sur;
+                    textBox3.Text = ath.Sport;
+                }
+            }
+
+        }
+
+        private static TextBox NewMethod()
+        {
+            return new TextBox();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Save();
+        }
+    }
+}
